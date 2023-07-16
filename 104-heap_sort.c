29ef255da7202018474da936e038b8de1d2a0fcc
dhev-wisdom <nonsowisdom@gmail.com>
2023-07-15 18:59:11 -0700

    betty 104-heap_sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -11,26 +11,30 @@
  */
 void sift_down(int *array, size_t size, size_t root, size_t end)
 {
-    size_t left_child, right_child, max_child;
-    int temp;
+	size_t left_child, right_child, max_child;
+	int temp;
 
-    while ((left_child = (2 * root) + 1) <= end) {
-        max_child = left_child;
-        right_child = left_child + 1;
+	while ((left_child = (2 * root) + 1) <= end)
+	{
+		max_child = left_child;
+		right_child = left_child + 1;
 
-        if (right_child <= end && array[right_child] > array[left_child])
-            max_child = right_child;
+		if (right_child <= end && array[right_child] > array[left_child])
+			max_child = right_child;
 
-        if (array[root] < array[max_child]) {
-            temp = array[root];
-            array[root] = array[max_child];
-            array[max_child] = temp;
-            print_array(array, size);
-            root = max_child;
-        } else {
-            break;
-        }
-    }
+		if (array[root] < array[max_child])
+		{
+			temp = array[root];
+			array[root] = array[max_child];
+			array[max_child] = temp;
+			print_array(array, size);
+			root = max_child;
+		}
+		else
+		{
+			break;
+		}
+	}
 }
 
 /**
@@ -41,10 +45,10 @@ void sift_down(int *array, size_t size, size_t root, size_t end)
  */
 void heapify(int *array, size_t size)
 {
-    ssize_t i;
+	ssize_t i;
 
-    for (i = (size / 2) - 1; i >= 0; i--)
-        sift_down(array, size, i, size - 1);
+	for (i = (size / 2) - 1; i >= 0; i--)
+		sift_down(array, size, i, size - 1);
 }
 
 /**
@@ -56,22 +60,22 @@ void heapify(int *array, size_t size)
  */
 void heap_sort(int *array, size_t size)
 {
-    size_t end;
-    int temp;
+	size_t end;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    heapify(array, size);
+	heapify(array, size);
 
-    end = size - 1;
-    while (end > 0) {
-        temp = array[0];
-        array[0] = array[end];
-        array[end] = temp;
-        print_array(array, size);
-        sift_down(array, size, 0, end - 1);
-        end--;
-    }
+	end = size - 1;
+	while (end > 0)
+	{
+		temp = array[0];
+		array[0] = array[end];
+		array[end] = temp;
+		print_array(array, size);
+		sift_down(array, size, 0, end - 1);
+		end--;
+	}
 }
-
